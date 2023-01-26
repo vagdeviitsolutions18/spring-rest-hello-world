24c83af94fd471728bfebc6fca9432e0997e1afe
vagdeviitsolutions18 <116752605+vagdeviitsolutions18@users.noreply.github.com>
2023-01-26 23:29:11 +0530

    added sql injection
    added sql injection to verify

--- src/main/java/com/vagdeviit/BookRepository.java
+++ src/main/java/com/vagdeviit/BookRepository.java
@@ -3,4 +3,12 @@ package com.vagdeviit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+  
+    @PersistenceContext
+    private EntityManager em;
+  
+    public String customFindMethod(String tableName) {
+       Query query = em.createNativeQuery("select id from "+tableName+" where username = chinna");
+      return "";
+    }
 }
