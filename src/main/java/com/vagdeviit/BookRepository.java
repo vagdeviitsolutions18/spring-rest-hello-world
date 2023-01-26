9b20c1516c0eeba3e15578259c04158e1bff0f65
vagdeviitsolutions18 <116752605+vagdeviitsolutions18@users.noreply.github.com>
2023-01-27 00:07:54 +0530

    Update BookRepository.java

--- src/main/java/com/vagdeviit/BookRepository.java
+++ src/main/java/com/vagdeviit/BookRepository.java
@@ -1,15 +1,7 @@
 package com.vagdeviit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import javax.persistence.EntityManager;
+
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-  
-    @PersistenceContext
-    private EntityManager em;
-  
-    public String customFindMethod(String tableName) {
-       Query query = em.createNativeQuery("select * from "+tableName);
-      return "";
-    }
 }
